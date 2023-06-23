87ab3fcab7af44b3f6ef25f9ba4369f287a01c8c
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2023-06-22 23:58:52 -0400

    add e configurando libs de auto import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+  vue({
+    reactivityTransform: true
+  }),
+  AutoImport({
+    imports: [
+      'vue'
+    ],
+    dts: true,
+    dirs: ['src/**']
+  }),
+  Components({
+    dts: true,
+  })]
+
 })
