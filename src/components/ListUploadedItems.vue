50686ce6623df1649369cfcdba1d9c0d25e544a4
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2023-06-24 02:44:54 -0400

    component for listing uploaded items

---
+++ src/components/ListUploadedItems.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-if="props.filesSelecteds.length > 0">
+    <div
+      v-for="({ name }, index) in filesSelecteds"
+      :key="index"
+      class="item-card"
+    >
+      <span>{{ name }}</span>
+      <MSWordIconVue class="icon-position" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import MSWordIconVue from "../assets/icons/MSWordIcon.vue";
+import { PropType } from "vue";
+import { IFile } from "../interfaces/IFiles";
+const props = defineProps({
+  filesSelecteds: Array as PropType<IFile[]>
+})
+</script>
+
+<style scoped>
+.item-card {
+  border: 1px solid var(--main-text-color);
+  padding: 0.5em;
+  margin: 0.5em;
+  border-radius: 0.2em;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: red;
+}</style>
