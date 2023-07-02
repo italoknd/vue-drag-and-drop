5ae00410d416d57110ddd690d61fcf48e103f1f0
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2023-07-02 12:45:06 -0400

    showing images on upload

--- src/components/ListUploadedItems.vue
+++ src/components/ListUploadedItems.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="uploaded-items">
-    <div v-for="({ name }, index) in props.selectedFiles" :key="index" class="item-card">
+    <div
+      v-for="({ name, dataURL }, index) in props.selectedFiles"
+      :key="index"
+      class="item-card"
+    >
       <div class="files-container" :title="name">
         <div id="icon-box">
-          <Icons :doctype="name" />
+          <Icons v-if="doesNotIncludes(name)" :doctype="name" />
+          <img class="miniature" v-else :src="dataURL" alt="Imagem..." />
         </div>
         <span>{{ name.length > 20 ? name.slice(0, 30) + "..." : name }}</span>
       </div>
@@ -31,6 +36,19 @@ const props = defineProps<{
 const removeFile = (idx: number) => {
   emit("removeFile", idx);
 };
+
+//FUNCTIONS
+const doesNotIncludes = (name: string): boolean => {
+  let includes: boolean = false;
+
+  if (
+    !name?.includes(".png") &&
+    !name?.includes(".jpg") &&
+    !name?.includes(".jpeg") &&
+    !name?.includes(".svg")
+  )
+    return (includes = true);
+};
 </script>
 
 <style scoped lang="scss">
@@ -38,6 +56,14 @@ const removeFile = (idx: number) => {
   border: 1px solid var(--secondary-bg-color);
   border-radius: 0.1125em;
   margin-top: 0.5em;
+  max-height: 10.625em;
+  overflow-y: scroll;
+
+  .miniature {
+    width: 1.7em;
+    height: 1.7em;
+    object-fit: cover;
+  }
 }
 .item-card {
   border: 1px solid var(--secondary-bg-color);
