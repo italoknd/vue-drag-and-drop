16cf954ceee84dad13fe6e5af59d6f103dcf0efb
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2023-12-18 01:05:54 -0400

    new base prop added to do the deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import ReactivityTransform from '@vue-macros/reactivity-transform/vite'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import ReactivityTransform from "@vue-macros/reactivity-transform/vite";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-  vue(),
-  ReactivityTransform(),
-  AutoImport({
-    imports: [
-      'vue',
-      'vue/macros',
-    ],
-    dts: true,
-    dirs: ['src/**']
-  }),
-  Components({
-    dts: true,
-    dirs: ["src/**/*.vue"]
-  })]
-
-})
+    vue(),
+    ReactivityTransform(),
+    AutoImport({
+      imports: ["vue", "vue/macros"],
+      dts: true,
+      dirs: ["src/**"],
+    }),
+    Components({
+      dts: true,
+      dirs: ["src/**/*.vue"],
+    }),
+  ],
+  base: "/vue-drag-and-drop",
+});
