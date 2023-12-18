102e99c06ecb92ea73c4c21755f0e8cebb9b76b2
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2023-12-18 00:13:43 -0400

    functions added to validate file type

--- src/components/Icons.vue
+++ src/components/Icons.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
-    <IconMSWord class="icon-position" v-if="doctype?.endsWith('.docx')" />
+    <IconMSWord class="icon-position" v-if="msWordTypeHandler()" />
     <IconMSExcel class="icon-position" v-if="doctype?.endsWith('.xlsx')" />
-    <IconMSPowerPoint class="icon-position" v-if="doctype?.endsWith('.pptx')"/>
+    <IconMSPowerPoint class="icon-position" v-if="doctype?.endsWith('.pptx')" />
     <IconTextFile class="icon-position" v-if="doctype?.endsWith('.txt')" />
-    <IconPDF class="icon-position" v-if="doctype?.endsWith('.pdf')"/>
+    <IconPDF class="icon-position" v-if="doctype?.endsWith('.pdf')" />
+    <IconMusic class="icon-position" v-if="audioTypeHandler()" />
   </div>
 </template>
 
@@ -13,13 +14,26 @@ import IconMSExcel from "../assets/icons/IconMSExcel.vue";
 import IconMSWord from "../assets/icons/IconMSWord.vue";
 import IconMSPowerPoint from "../assets/icons/IconMSPowerPoint.vue";
 import IconTextFile from "../assets/icons/IconTextFile.vue";
-import IconPDF from "../assets/icons/IconPDF.vue"
+import IconPDF from "../assets/icons/IconPDF.vue";
+import IconMusic from "../assets/icons/IconAudio.vue";
 
 const props = defineProps<{
-  doctype: String,
+  doctype: String;
 }>();
 
-const doctype = $computed<String>(()=>{
-  return props.doctype
-})
+const doctype = $computed<String>(() => {
+  return props.doctype;
+});
+
+const audioTypeHandler = () => {
+  if (doctype?.endsWith(".mp3") || doctype?.endsWith(".mp4")) {
+    return true;
+  }
+};
+
+const msWordTypeHandler = () => {
+  if (doctype?.endsWith(".docx") || doctype?.endsWith(".doc")) {
+    return true;
+  }
+};
 </script>
